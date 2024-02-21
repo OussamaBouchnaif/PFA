4647efe154e1b0a103cf5a851aec147dcdec106c
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-02-21 15:08:48 +0100

    Update Client.php
    update client

--- client/src/Entity/Client.php
+++ client/src/Entity/Client.php
@@ -59,11 +59,8 @@ class Client extends Personne
         $this->avisCameras = new ArrayCollection();
         $this->favoritCameras = new ArrayCollection();
         $this->commandes = new ArrayCollection();
-<<<<<<< Updated upstream
         $this->roles[] = 'client';
-=======
-        
->>>>>>> Stashed changes
+
     }
 
     public function getRole()
