6e6b92e1506baf7749c60348d2582358733b4777
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-02-26 18:59:43 +0100

    data

--- admin/src/Controller/CameraController.php
+++ admin/src/Controller/CameraController.php
@@ -21,7 +21,7 @@ class CameraController extends AbstractController
     #[Route('/', name: 'ds')]
     public function dash(): Response
     {
-        return $this->render('admin/addProduct.html.twig');
+        return $this->render('admin/test.html.twig');
     }
 
     #[Route('/camera/details/{id}', name: 'camera_details')]
