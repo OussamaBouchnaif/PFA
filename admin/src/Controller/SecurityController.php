a57e2d2e1dd15f754b583ac18b34c4679bd3537e
Yassine Chidmi <131216380+yassinechidm@users.noreply.github.com>
2024-03-10 19:22:40 +0000

    fixer ahthentification partie admin

--- admin/src/Controller/SecurityController.php
+++ admin/src/Controller/SecurityController.php
@@ -2,31 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ClientType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/signup', name: 'app_signup')]
+    public function signup(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // Créez une nouvelle instance de l'entité User
+        $user = new User();
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        // Créez le formulaire en utilisant la classe ClientType et l'objet User
+        $form = $this->createForm(ClientType::class, $user);
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // Gérez la requête
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encodez le mot de passe
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword() // Utilisez la méthode getPassword() pour récupérer le mot de passe depuis l'objet User
+                )
+            );
+
+            // Enregistrez l'utilisateur dans la base de données
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigez l'utilisateur vers la page de connexion après un enregistrement réussi
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Affichez le formulaire de création de compte
+        return $this->render('security/signup.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route('/login', name: 'app_login')]
+    public function login(Request $request, \Symfony\Component\Security\Http\Authentication\AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupérez l'erreur de connexion
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Récupérez le dernier nom d'utilisateur saisi par l'utilisateur
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Affichez le formulaire de connexion
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
+        // Cette méthode peut rester vide car elle sera interceptée par le pare-feu de sécurité pour la déconnexion de l'utilisateur
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
--- admin/src/Entity/User.php
+++ admin/src/Entity/User.php
@@ -2,38 +2,21 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Personne;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-
-class User
+class User extends Personne
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 20)]
-    private ?string $nom = null;
-
-    #[ORM\Column(length: 20)]
-    private ?string $prenom = null;
-
-    #[ORM\Column]
-    private array $roles = [];
-
-    #[ORM\Column(length: 70)]
-    private ?string $email = null;
-
-    #[ORM\Column(length: 50)]
-    private ?string $password = null;
-
-    #[ORM\Column(length: 10)]
-    private ?string $numtele = null;
 
     #[ORM\Column(length: 100, nullable: true)]
     private ?string $image = null;
@@ -54,87 +37,8 @@ class User
         return $this->id;
     }
 
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): static
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): static
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function getNumtele(): ?string
-    {
-        return $this->numtele;
-    }
-
-    public function setNumtele(string $numtele): static
-    {
-        $this->numtele = $numtele;
-
-        return $this;
-    }
-
+   
     
-
-    /**
-     * Get the value of roles
-     */ 
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-
-    /**
-     * Set the value of roles
-     *
-     * @return  self
-     */ 
-    public function setRoles($roles)
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
 
     /**
      * Get the value of image
@@ -204,5 +108,9 @@ class User
         }
 
         return $this;
+    }
+    public function getUserIdentifier(): string
+    {
+        return '';
     }
 }
