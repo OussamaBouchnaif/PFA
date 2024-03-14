1a61a1572f9cb2da14a7fb5959aee70e125e2a49
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-03-14 22:04:41 +0100

    maintain code

--- client/src/Controller/DetailsController.php
+++ client/src/Controller/DetailsController.php
@@ -6,18 +6,12 @@ use App\Entity\AvisCamera;
 use App\Entity\Camera;
 use App\Factory\Factory;
 use App\Forms\AvisType;
-use Doctrine\ORM\EntityManager;
 use Symfony\UX\Turbo\TurboBundle;
-use App\Repository\CameraRepository;
 use App\Repository\AvisCameraRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Service\Api\CallApiCameraService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Validator\Constraints\Date;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DetailsController extends AbstractController
@@ -39,7 +33,6 @@ class DetailsController extends AbstractController
                 return $this->render('client/pages/components/avis.stream.html.twig', ['content' => $form->getData()['content'],'name'=>$this->getUser()->getNom(),'createdAt' => new \DateTimeImmutable()]); // Pass just the content string
             }
         }
-
         return $this->render('client/pages/product-details.html.twig', [
             'camera' => $callapi->getCameraById($camera->getId()),
             'form' => $form->createView(),
