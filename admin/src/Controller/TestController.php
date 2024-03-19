38848889adbd381d43dad40580351c588ccd79a3
Yassine Chidmi <131216380+yassinechidm@users.noreply.github.com>
2024-03-19 00:04:11 +0000

    test image

--- admin/src/Controller/TestController.php
+++ admin/src/Controller/TestController.php
@@ -1,19 +1,14 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Camera;
 use App\Form\CameraType;
 use App\Entity\ImageCamera;
 use App\Form\PhotoType;
-use App\Repository\CameraRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\ImageCameraRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TestController extends AbstractController
@@ -45,11 +40,11 @@ class TestController extends AbstractController
                 // Association de l'image à la caméra
                 $image->setCamera($camera);
 
-                // Persist et flush de l'image
+                // Persist de l'image
                 $entityManager->persist($image);
             }
 
-            // Persist et flush de la caméra
+            // Persist de la caméra
             $entityManager->persist($camera);
             $entityManager->flush();
 
