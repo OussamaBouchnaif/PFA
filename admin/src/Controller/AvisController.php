2af0b5a0c5f55ab8848b61500cedbf0c8ce90bfe
aminebc <aminemohammedbachri@gmail.com>
2024-04-14 23:35:50 +0100

    avis controller modifications

--- admin/src/Controller/AvisController.php
+++ admin/src/Controller/AvisController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\AvisCamera; // Import pour AvisCamera
+use Doctrine\ORM\EntityManagerInterface; // Import pour EntityManagerInterface
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class AvisController extends AbstractController
 {
     #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer tous les avis depuis la base de données
+        $avisRepository = $entityManager->getRepository(AvisCamera::class);
+        $avis_list = $avisRepository->findAll();
+
         return $this->render('avis/index.html.twig', [
             'controller_name' => 'AvisController',
+            'avis_list' => $avis_list, // Utilisez "avis_list" pour correspondre au modèle Twig
         ]);
     }
 }
