79c9285b3edb64351462c22838efa53d712008a8
Yassine Chidmi <131216380+yassinechidm@users.noreply.github.com>
2024-04-27 14:14:46 +0100

    clean template

--- admin/config/bundles.php
+++ admin/config/bundles.php
@@ -20,4 +20,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
---
+++ admin/src/Form/LigneReductionTypeForm.php
@@ -0,0 +1,36 @@
+<?php
+// src/Form/LigneReductionType.php
+namespace App\Form;
+
+use App\Entity\LigneReduction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Camera;
+use App\Entity\Reduction;
+
+class LigneReductionTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('camera', EntityType::class, [
+                'class' => Camera::class,
+                'choice_label' => 'nom', // Choisir le champ à afficher comme label
+                'placeholder' => 'Choisir une caméra', // Texte par défaut du champ select
+            ])
+            ->add('reduction', EntityType::class, [
+                'class' => Reduction::class,
+                'choice_label' => 'description', // Choisir le champ à afficher comme label
+                'placeholder' => 'Choisir une réduction', // Texte par défaut du champ select
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => LigneReduction::class,
+        ]);
+    }
+}
