c0782c6a566cc165c8319b8cefc702389fbb4e3f
aminebc <aminemohammedbachri@gmail.com>
2024-05-07 01:20:13 +0100

    avis delet button

--- admin/src/Controller/AvisController.php
+++ admin/src/Controller/AvisController.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Controller;
 
 use App\Entity\AvisCamera;
@@ -13,7 +14,6 @@ class AvisController extends AbstractController
     #[Route('/avis', name: 'app_avis')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-
         $avisRepository = $entityManager->getRepository(AvisCamera::class);
         $avis_list = $avisRepository->findAll();
 
@@ -21,5 +21,20 @@ class AvisController extends AbstractController
             'controller_name' => 'AvisController',
             'avis_list' => $avis_list,
         ]);
+    }
+
+    #[Route('/avis/delete/{id}', name: 'app_avis_delete')]
+    public function delete(AvisCamera $avis, EntityManagerInterface $entityManager): Response
+    {
+        try {
+            $entityManager->remove($avis);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'avis a été supprimé avec succès.');
+        } catch (\Exception $e) {
+            $this->addFlash('error', 'Une erreur s\'est produite lors de la suppression de l\'avis.');
+        }
+
+        return $this->redirectToRoute('app_avis');
     }
 }
