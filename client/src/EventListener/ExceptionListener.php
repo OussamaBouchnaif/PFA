b0ec075e8e8c369b426bb00c7782916dbf585eb8
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-05-26 16:18:32 +0200

    maintain code

--- client/src/EventListener/ExceptionListener.php
+++ client/src/EventListener/ExceptionListener.php
@@ -23,17 +23,13 @@ class ExceptionListener
         $exception = $event->getThrowable();
 
         if ($exception instanceof NotFoundHttpException) {
-            // Customize your response object to display the exception details
             $response = new Response(
                 $this->twig->render('client/exceptions/404.html.twig', ['exception' => $exception])
             );
 
-            // Http code for not found
-            $response->setStatusCode(Response::HTTP_NOT_FOUND);
 
-            // Send the modified response object to the event
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
             $event->setResponse($response);
         }
     }
-
 }
